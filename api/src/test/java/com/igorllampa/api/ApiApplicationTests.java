c6b651173dd50e227d4f5df631d7fa0212d91f19
igorl <igorllampa@gmail.com>
2024-07-30 09:26:32 -0300

    set up initial packages for project organization

---
+++ api/src/test/java/com/igorllampa/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igorllampa.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
