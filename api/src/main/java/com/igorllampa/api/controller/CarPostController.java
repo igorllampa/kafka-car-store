986c2a08736cf9e9620045920fe3be3efddd4402
igorl <igorllampa@gmail.com>
2024-07-31 21:21:57 -0300

    Add controllers for car post and owner post, and also kafka configuration class

--- api/src/main/java/com/igorllampa/api/controller/CarPostController.java
+++ api/src/main/java/com/igorllampa/api/controller/CarPostController.java
@@ -1,2 +1,45 @@
-package com.igorllampa.api.controller;public class CarPostController {
+package com.igorllampa.api.controller;
+
+import com.igorllampa.api.dto.CarPostDTO;
+import com.igorllampa.api.message.KafkaProducerMessage;
+import com.igorllampa.api.service.CarPostStoreService;
+import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/car")
+public class CarPostController {
+
+    @Autowired
+    private CarPostStoreService carPostStoreService;
+
+    private KafkaProducerMessage kafkaProducerMessage;
+
+    @PostMapping("/posts")
+    public ResponseEntity postCarForSale(@RequestBody CarPostDTO carPostDTO){
+        kafkaProducerMessage.sendMessage(carPostDTO);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/posts")
+    public ResponseEntity<List<CarPostDTO>> getCarSales(){
+        List<CarPostDTO> carForSales = carPostStoreService.getCarForSales();
+        return ResponseEntity.status(HttpStatus.FOUND).body(carForSales);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity changeCarSale(@RequestBody CarPostDTO carPostDTO, @PathVariable("id") String id){
+        carPostStoreService.changeCarForSale(carPostDTO, id);
+        return ResponseEntity.ok().build();
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteCarForSale(@PathVariable("id") String id){
+        carPostStoreService.removeCarForSale(id);
+        return ResponseEntity.ok().build();
+    }
 }
--- api/src/main/java/com/igorllampa/api/controller/OwnerPostController.java
+++ api/src/main/java/com/igorllampa/api/controller/OwnerPostController.java
@@ -1,2 +1,24 @@
-package com.igorllampa.api.controller;public class OwnerPostController {
+package com.igorllampa.api.controller;
+
+import com.igorllampa.api.dto.OwnerPostDTO;
+import com.igorllampa.api.service.OwnerPostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/owner")
+public class OwnerPostController {
+
+    @Autowired
+    private OwnerPostService ownerPostService;
+
+    @PostMapping
+    public ResponseEntity createOwnerCar(@RequestBody OwnerPostDTO ownerPostDTO){
+        ownerPostService.createOwnerCar(ownerPostDTO);
+        return ResponseEntity.ok().build();
+    }
 }
